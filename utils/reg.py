8f8db51c2c016d563cd2881c2e9cb98194511e1e
com <ksl13579@hanmail.net>
2021-05-03 14:25:56 +0900

    reg 수정

--- utils/reg.py
+++ utils/reg.py
@@ -1,13 +1,26 @@
 def tokenize(text):
     import re
+    if not texts:
+        return
     
-    URL_PATTERN = re.compile('((http|https|ftp):\/\/)?(www.)?[a-zA-Z0-9-_]+\.[a-zA-Z]+(\/[a-zA-Z0-9-_.=&/가-힣]+)?')
-    MULTIPLE_CHAR = re.compile(r'(.)\1+')
-    
-    text = (re.sub(URL_PATTERN, 'url',text))
-    text = re.sub(MULTIPLE_CHAR,r'\1\1',text)
-    return text
-    
+    answer = []
+    for text in texts:
+        if not isinstance(text,str):
+            continue
+        URL_PATTERN = re.compile('((http|https|ftp):\/\/)?(www.)?[a-zA-Z0-9-_]+\.[a-zA-Z]+(\/[a-zA-Z0-9-_.=&/가-힣]+)?')
+        DATE_PATTERN = re.compile(r"(\d{2,4})(\.|\/|\s)(\d{1,2})(\.|\/|\s)(\d{1,2})(\.|\/|\s)?([월화수목금토일])?(요일)? ")
+        TEL_PATTERN = re.compile("(\d{2,3})(-|\.| )+(\d{3,4})(-|\.| )+(\d{3,4})")
+        EMAIL_PATTERN = re.compile('([a-zA-Z0-9]+)@([a-zA-Z0-9]+)\.([a-z]{2,3})')
+        MULTIPLE_CHAR = re.compile(r'(.)\1+')
+
+        text = re.sub(URL_PATTERN, 'URL',text)
+        text = re.sub(DATE_PATTERN,'DATETIME',text)
+        text = re.sub(TEL_PATTERN, 'TEL ', text)
+        text = re.sub(EMAIL_PATTERN, 'EMAIL', text)
+        text = re.sub(MULTIPLE_CHAR, r'\1\1',text)
+        answer.append(text)
+    return answer
+
 def read_korquad():
     with open(glob.glob(PATH)[0],'r',encoding='utf-8') as f, open('/home/data/data/KorQuAD/process/process_korquad_train.txt','w',encoding='utf-8') as f2:
     dataset_json = json.loads(f.read())
